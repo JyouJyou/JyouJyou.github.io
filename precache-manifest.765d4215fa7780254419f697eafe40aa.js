b02b4d74b647304c92c7da43406f904cc2239063
Eddy <eddy.chi.chu@gmail.com>
2021-01-24 00:21:43 +0800

    Updates

---
+++ precache-manifest.765d4215fa7780254419f697eafe40aa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b8616533a3336d39e733431fb0ca2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45cbae565159bf9de65d",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "d061d860c0d1f5a18adf",
+    "url": "/static/css/main.aea40c67.chunk.css"
+  },
+  {
+    "revision": "45cbae565159bf9de65d",
+    "url": "/static/js/2.4a0a51dd.chunk.js"
+  },
+  {
+    "revision": "cfe00a0c9c27ae1b210e6b01a6904cd7",
+    "url": "/static/js/2.4a0a51dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d061d860c0d1f5a18adf",
+    "url": "/static/js/main.1be91f49.chunk.js"
+  },
+  {
+    "revision": "f860afacb5593b378c8a",
+    "url": "/static/js/runtime-main.e5aa723d.js"
+  },
+  {
+    "revision": "3644ee708bf9efe747ea78b2965ca58f",
+    "url": "/static/media/bg.3644ee70.jpg"
+  }
+]);
