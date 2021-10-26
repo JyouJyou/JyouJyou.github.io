105f0e0cf7eb265863cdb9ab14a3df78a5b0bf13
Eddie Hsieh <yida8627@gmail.com>
2021-10-26 20:07:37 +0800

    Fix to Hash Router for github bug issue

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,10 @@
 export function textColorMapper(text) {}
 
-export const URL_BASE = "/#";
+export const URL_BASE = "";
 
 export const URL_TOP = URL_BASE + "/#page-top";
 
-export const URL_ABOUT = "/#about";
+export const URL_ABOUT = URL_BASE + "/#about";
 
 export const URL_PRODUCTS = URL_BASE + "/#products";
 
@@ -14,10 +14,10 @@ export const URL_TEAM = URL_BASE + "/#team";
 
 export const URL_CONTACT = URL_BASE + "/#contact";
 
-export const URL_INSIGHTS = URL_BASE + "/insights";
+export const URL_INSIGHTS = URL_BASE + "/#/insights";
 
-export const URL_BUSINESS_MODEL = URL_BASE + "/business-model";
+export const URL_BUSINESS_MODEL = URL_BASE + "/#/business-model";
 
-export const URL_WHY_CHOOSE_IPC = URL_BASE + "/why-choose-ipc";
+export const URL_WHY_CHOOSE_IPC = URL_BASE + "/#/why-choose-ipc";
 
-export const URL_BCTECH_BROCHURE = URL_BASE + "/bc-tech-brochure";
+export const URL_BCTECH_BROCHURE = URL_BASE + "/#/bc-tech-brochure";
